23a47421042258d6536956e8963692ea157f8146
Charley Wu <charleymswu@gmail.com>
2021-04-16 16:03:23 +0200

    updated db find tool

--- dbfind.py
+++ dbfind.py
@@ -2,8 +2,10 @@
 # Charley Wu, April 2021
 import pymongo
 import os
+from pathlib import Path
 
-keyfile = '6_database/credentials.key'
+keypath = (Path(__file__).parent / "6_database/").resolve()
+keyfile = os.path.join(keypath,'credentials.key')
 creds = open(keyfile, "r").read().splitlines()
 myclient = pymongo.MongoClient('134.76.24.75', username=creds[0], password=creds[1], authSource='ohol') 
 db = myclient.ohol
@@ -13,16 +15,17 @@ db = myclient.ohol
 #player
 #community
 
-def item(id, fields=['name']):
-	"""Look up what an item id refers to or a list of ids
+def item(query, target='id', fields=['name']):
+	"""Look up what an item id (or item name, etc...) refers to or a list of ids
+		target specifies the field you want to query, and query is the target
 	 You can specify a list of field names to return
 	 The default to return just the 'name' field
 	 Or an empty list returns all fields
 	"""
 	output = []
-	if isinstance(id, list):
+	if isinstance(query, list):
 		#If id is a list
-		query = db.objects.find({'id':id})
+		query = db.objects.find({target:query})
 		for q in query:
 			if len(fields)==0:
 				output.append(q) #Return everything if fields is empty
@@ -30,7 +33,7 @@ def item(id, fields=['name']):
 				output.append([q[f] for f in fields])
 	else:
 		#Single id case
-		query = db.objects.find({'id':id})
+		query = db.objects.find({target:query})
 		for q in query:
 			if len(fields)==0:
 				output.append(q) #Return everything if fields is empty
