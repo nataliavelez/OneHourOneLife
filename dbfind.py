45ba029a651e98419ea4ef4cdab0e699f596093b
Charley Wu <charleymswu@gmail.com>
2021-04-21 16:22:42 +0200

    added transitions and fixed list queries in dbfind

--- dbfind.py
+++ dbfind.py
@@ -11,6 +11,7 @@ myclient = pymongo.MongoClient('134.76.24.75', username=creds[0], password=creds
 db = myclient.ohol
 
 #TODO:
+#List of queries is still not working
 #avatar
 #player
 #community
@@ -25,7 +26,7 @@ def item(query, target='id', fields=['name']):
 	output = []
 	if isinstance(query, list):
 		#If id is a list
-		query = db.objects.find({target:query})
+		query = db.objects.find({target:{"$in":query}})
 		for q in query:
 			if len(fields)==0:
 				output.append(q) #Return everything if fields is empty
@@ -41,3 +42,35 @@ def item(query, target='id', fields=['name']):
 				output.append([q[f] for f in fields])
 	return(output)
 
+
+def transition(query, direction='downstream', fields = []):
+	'''Query the upstream or downstream items based on allowed transitions
+	'''
+	output = []
+	#define query target and fields to return based on downstream vs. upstream option
+	if direction=='downstream':
+		target = 'origTarget'
+		if len(fields)==0:
+			fields = ['newTarget']
+	elif  direction == 'upstream':
+		target = 'newTarget'
+		if len(fields)==0:
+			fields = ['origTarget']
+	#are we querying a single target or a list?
+	if isinstance(query, list):
+		#If id is a list
+		query = db.transitions.find({target:{"$in":query}})
+		for q in query:
+			if len(fields)==0:
+				output.append(q) #Return everything if fields is empty
+			else:
+				output.append([q[f] for f in fields])
+	else:
+		#Single id case
+		query = db.transitions.find({target:query})
+		for q in query:
+			if len(fields)==0:
+				output.append(q) #Return everything if fields is empty
+			else:
+				output.append([q[f] for f in fields])
+	return(output)
