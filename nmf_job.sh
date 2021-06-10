71b5dcaed42d220a62449544039bdbcdede6b612
Charley Wu <charleymswu@gmail.com>
2021-06-10 20:40:34 +0200

    updated job script

--- nmf_job.sh
+++ nmf_job.sh
@@ -3,12 +3,12 @@
 for inputMatrix in cleaned tfidf ; do
 	for numDims in $(seq -w 5 30) ; do
 	    echo '#!/bin/bash'                    > job.slurm
-	    echo "#SBATCH --job-name nmf_$inputMatrix_$numDims"  >> job.slurm
+	    echo "#SBATCH --job-name nmf_${inputMatrix}_${numDims}"  >> job.slurm
 	    echo "#SBATCH --time 6:0:0"              >> job.slurm
 	    echo "#SBATCH --mem 42g"              >> job.slurm
 	    echo "#SBATCH --cpus-per-task 1"              >> job.slurm
 	    echo "#SBATCH --workdir ."              >> job.slurm
-	    echo "#SBATCH -o /home/mpib/cwu/OneHourOneLife/logs/nmf_$inputMatrix_$numDims.out"              >> job.slurm
+	    echo "#SBATCH -o /home/mpib/cwu/OneHourOneLife/logs/nmf_${inputMatrix}_${numDims}.out"              >> job.slurm
 	    echo "module load virtualenvwrapper"              >> job.slurm
 	    echo "workon ohol"              >> job.slurm
 	    echo "python jobspace/validate_nmf.py -d $numDims -m $inputMatrix"           >> job.slurm
