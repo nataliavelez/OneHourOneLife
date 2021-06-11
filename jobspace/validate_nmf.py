c7ba4f35ffb03198ef976e798bda060d5529bd09
Charley Wu <charleymswu@gmail.com>
2021-06-11 14:51:24 +0200

    ready to run tfidf validation

--- jobspace/validate_nmf.py
+++ jobspace/validate_nmf.py
@@ -34,63 +34,66 @@ db = dbfind.db
 # jobMatrix = argsdict['matrix']
 
 #Running it sequentially
-for jobMatrix in ('cleaned', 'tfidf'):
-    #Which job matrix to use?
-    if jobMatrix == 'cleaned':
-        #Load cleaned matrix. This takes about 30 seconds
-        mat_id = list(db.cleaned_job_matrix.files.find())[0]['_id'] #get id
-        fs = gridfs.GridFS(db, collection='cleaned_job_matrix') 
-        mat_bin = fs.get(mat_id) #extract binary
-        mat_dict = pickle.load(mat_bin, encoding='latin1')
-        jobMatrixCleaned = mat_dict['mat']
-        itemIds = mat_dict['items']
-        avatarIds = mat_dict['avatars']
-        print('Loaded Job matrix:')
-        print(jobMatrixCleaned.shape)
-        dat = jobMatrixCleaned
-    elif jobMatrix == 'tfidf':
-        #Load tfidf matrix. This takes about 30 seconds
-        mat_id = list(db.tfidf_matrix.files.find())[0]['_id'] #get id
-        fs = gridfs.GridFS(db, collection='tfidf_matrix') 
-        mat_bin = fs.get(mat_id) #extract binary
-        mat_dict = pickle.load(mat_bin, encoding='latin1')
-        tfidfJobMatrixNormalized = mat_dict['mat']
-        itemIds = mat_dict['items']
-        avatarIds = mat_dict['avatars']
-        print('Loaded Job matrix:')
-        print(tfidfJobMatrixNormalized.shape)
-        dat = tfidfJobMatrixNormalized
-    #Loop over different numbers of latent dimensions
-    for dims in range(5,30+1):
-        print("\n\n Starting analysis: %s %i \n\n" % (jobMatrix,dims))
-        #Fit model
-        #dat = dat[1:10000,:] #debug for testing on smaller data set=
-        model = NMF(n_components=dims, init='random', random_state=0).fit(dat)
-        VarExplained = metrics.explained_variance_score(dat.toarray(), model.inverse_transform(model.transform(dat))) #variance explained
-        W = model.transform(dat) #Avatar embeddings
-        H = model.components_ #item embeddings
+#for jobMatrix in ('cleaned', 'tfidf'):
 
-        #Save to database: Too large to save the full matrices, so I will only export the var explained for now
-        #output = {'W': pickle.dumps(csr_matrix(W), protocol=2), 'H':pickle.dumps(csr_matrix(H), protocol=2), 'varExplained':VarExplained, 'n_components':dims, 'jobMatrix': jobMatrix}
-        output = {'varExplained':VarExplained, 'n_components':dims, 'jobMatrix': jobMatrix}
+jobMatrix = 'tfidf'
 
-        #SAVE to DATABASE
-        #db.nmf_validation.drop() #DELETES ALL DATA
-        db.nmf_validation.insert(output)
+#Which job matrix to use?
+if jobMatrix == 'cleaned':
+    #Load cleaned matrix. This takes about 30 seconds
+    mat_id = list(db.cleaned_job_matrix.files.find())[db.cleaned_job_matrix.files.find().count()-1]['_id'] #get id
+    fs = gridfs.GridFS(db, collection='cleaned_job_matrix') 
+    mat_bin = fs.get(mat_id) #extract binary
+    mat_dict = pickle.load(mat_bin, encoding='latin1')
+    jobMatrixCleaned = mat_dict['mat']
+    itemIds = mat_dict['items']
+    avatarIds = mat_dict['avatars']
+    print('Loaded Job matrix:')
+    print(jobMatrixCleaned.shape)
+    dat = jobMatrixCleaned
+elif jobMatrix == 'tfidf':
+    #Load tfidf matrix. This takes about 30 seconds
+    mat_id = list(db.tfidf_matrix.files.find())[db.tfidf_matrix.files.find().count()-1]['_id'] #get id
+    fs = gridfs.GridFS(db, collection='tfidf_matrix') 
+    mat_bin = fs.get(mat_id) #extract binary
+    mat_dict = pickle.load(mat_bin, encoding='latin1')
+    tfidfJobMatrixNormalized = mat_dict['mat']
+    itemIds = mat_dict['items']
+    avatarIds = mat_dict['avatars']
+    print('Loaded Job matrix:')
+    print(tfidfJobMatrixNormalized.shape)
+    dat = tfidfJobMatrixNormalized
+#Loop over different numbers of latent dimensions
+for dims in range(5,30+1):
+    print("\n\n Starting analysis: %s %i \n\n" % (jobMatrix,dims))
+    #Fit model
+    #dat = dat[1:10000,:] #debug for testing on smaller data set=
+    model = NMF(n_components=dims, init='random', random_state=0).fit(dat)
+    VarExplained = metrics.explained_variance_score(dat.toarray(), model.inverse_transform(model.transform(dat))) #variance explained
+    W = model.transform(dat) #Avatar embeddings
+    H = model.components_ #item embeddings
 
-        #Which items most strongly defined each component?
-        filepath = 'jobspace/outputs/%s/%i.txt' %  (jobMatrix,dims)
-        with open(filepath, 'w') as outfile:
-            for C in range(H.shape[0]):
-                comp = H[C,] #component
-                items = comp.argsort()[-10:][::-1]#sort by weight; select the top 10 items
-                outfile.write('%i\n' % C)
-                for item in items:
-                    try:
-                        objname = dbfind.item(itemIds[item])[0][0] #relate to item id and then look up
-                        weight = str(comp[item])
-                        outfile.write(' : '.join([objname, weight])+'\n')
-                    except:
-                        outfile.write(' : '.join([str(int(item)), str(comp[item])])+'\n')
-                outfile.write('\n')
+    #Save to database: Too large to save the full matrices, so I will only export the var explained for now
+    #output = {'W': pickle.dumps(csr_matrix(W), protocol=2), 'H':pickle.dumps(csr_matrix(H), protocol=2), 'varExplained':VarExplained, 'n_components':dims, 'jobMatrix': jobMatrix}
+    output = {'varExplained':VarExplained, 'n_components':dims, 'jobMatrix': jobMatrix}
+
+    #SAVE to DATABASE
+    #db.nmf_validation.drop() #DELETES ALL DATA
+    db.nmf_validation.insert(output)
+
+    #Which items most strongly defined each component?
+    filepath = 'jobspace/outputs/%s/%i.txt' %  (jobMatrix,dims)
+    with open(filepath, 'w') as outfile:
+        for C in range(H.shape[0]):
+            comp = H[C,] #component
+            items = comp.argsort()[-10:][::-1]#sort by weight; select the top 10 items
+            outfile.write('%i\n' % C)
+            for item in items:
+                try:
+                    objname = dbfind.item(itemIds[item])[0][0] #relate to item id and then look up
+                    weight = str(comp[item])
+                    outfile.write(' : '.join([objname, weight])+'\n')
+                except:
+                    outfile.write(' : '.join([str(int(item)), str(comp[item])])+'\n')
+            outfile.write('\n')
 
