349c7bf1160c4243d1c1f40ab0d84a8af4efef5c
charleywu <cwu@tardis.mpib-berlin.mpg.de>
2021-06-17 09:46:31 +0200

    logs from failed job

--- nmf_job.sh
+++ nmf_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name NMF
-#SBATCH --time 36:0:0
+#SBATCH --time 24:0:0
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 60g
 #SBATCH -o /home/mpib/cwu/OneHourOneLife/logs/nmf.out
