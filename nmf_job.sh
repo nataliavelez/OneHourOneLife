4fc7861cb2d7bb888d801ce01b8c03880e373448
Charley Wu <charleymswu@gmail.com>
2021-06-18 10:12:29 +0200

    ready to rerun validation

--- nmf_job.sh
+++ nmf_job.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name NMF
-#SBATCH --time 24:0:0
+#SBATCH --job-name NMF-random
+#SBATCH --time 48:0:0
+#SBATCH --partition long
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 60g
 #SBATCH -o /home/mpib/cwu/OneHourOneLife/logs/nmf.out
 #SBATCH --workdir .
 
-python -u jobspace/validate_nmf.py > logs/validate.out
+python -u jobspace/validate_nmf.py -m random > logs/random.out
