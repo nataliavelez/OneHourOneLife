70ec2fd0e23780125957766d19cd2a2f72450d29
Charley Wu <charleymswu@gmail.com>
2021-06-18 10:35:16 +0200

    Ready to run remaining dims for cleaned matrix

--- nmf_job.sh
+++ nmf_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name NMF-random
+#SBATCH --job-name NMF-cleaned
 #SBATCH --time 48:0:0
 #SBATCH --partition long
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 60g
-#SBATCH -o /home/mpib/cwu/OneHourOneLife/logs/nmf-random.out
+#SBATCH -o /home/mpib/cwu/OneHourOneLife/logs/nmf-cleaned.out
 #SBATCH --workdir .
 
-python -u jobspace/validate_nmf.py -m random > logs/random.out
+python -u jobspace/validate_nmf.py -m cleaned > logs/cleaned.out
