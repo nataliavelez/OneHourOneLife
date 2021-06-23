cd13c58b2438ffcbf1c935ad10555413a07f2b5d
charleywu <cwu@tardis.mpib-berlin.mpg.de>
2021-06-23 11:14:42 +0200

    models finished

--- nmf_job.sh
+++ nmf_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name NMF-cleaned
+#SBATCH --job-name NMF-tfidf
 #SBATCH --time 48:0:0
 #SBATCH --partition long
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 60g
-#SBATCH -o /home/mpib/cwu/OneHourOneLife/logs/nmf-cleaned.out
+#SBATCH -o /home/mpib/cwu/OneHourOneLife/logs/nmf-tfidf.out
 #SBATCH --workdir .
 
-python -u jobspace/validate_nmf.py -m cleaned > logs/cleaned.out
+python -u jobspace/validate_nmf.py -m tfidf > logs/tfidf.out
