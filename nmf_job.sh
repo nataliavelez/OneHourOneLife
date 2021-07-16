f3e6e633df493b22d76f95941b4b8633b36ad7c9
charleywu <cwu@tardis.mpib-berlin.mpg.de>
2021-07-16 17:59:30 +0200

    some validation results

--- nmf_job.sh
+++ nmf_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name NMF-tfidf
+#SBATCH --job-name NMF-random
 #SBATCH --time 48:0:0
 #SBATCH --partition long
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 60g
-#SBATCH -o /home/mpib/cwu/OneHourOneLife/logs/nmf-tfidf.out
+#SBATCH -o /home/mpib/cwu/OneHourOneLife/logs/nmf-random.out
 #SBATCH --workdir .
 
-python -u jobspace/validate_nmf.py -m tfidf > logs/tfidf.out
+python -u jobspace/validate_nmf.py -m random > logs/random.out
